adaea71ecf20fe9f6d306ceccbf9adf7740b2f14
Amphakarn <pisuthigomol@yahoo.com>
2020-12-30 21:43:58 +0000

    add comment

--- timer1.js
+++ timer1.js
@@ -1,10 +1,12 @@
+// To test, run "node timer1.js 10 3 5 15 9" in the terminal
+
 const timer = process.argv.slice(2);
 if (timer.length >= 1) {
   for (let i of timer) {
     i = Number(i);
-    if (i >= 0 && typeof i === "number") {
+    if (i >= 0 && typeof i === 'number') {
       setTimeout(() => {
-        process.stdout.write(`BEEP!\n`);
+        process.stdout.write('BEEP!\n');
       }, i * 1000);
     }
   }
